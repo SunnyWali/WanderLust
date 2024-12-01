654d9f4a67623e0f0315372caba8a1da5b2da845
Sunny Wali <sunnywali777@gmail.com>
2024-12-01 19:20:12 +0530

    made some changes in show.ejs

--- app.js
+++ app.js
@@ -1,98 +1,99 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const path = require("path");
-const Listing = require("../WanderLust/models/listing");
-const methodOverride=require("method-override");
-const ejsMate=require("ejs-mate");
-app.use(methodOverride("_method"));
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-app.use(express.urlencoded({ extended: true }));
-app.engine("ejs",ejsMate);
-app.use(express.static(path.join(__dirname,"public")));
-const mongoUrl = "mongodb://127.0.0.1:27017/WanderLust";
-app.listen(8080, (req, res) => {
-    console.log("Server is working on the port no 8080");
-});
+    const express = require("express");
+    const app = express();
+    const mongoose = require("mongoose");
+    const path = require("path");
+    const Listing = require("../WanderLust/models/listing");
+    const methodOverride=require("method-override");
+    const ejsMate=require("ejs-mate");
+    app.use(methodOverride("_method"));
+    app.set("view engine", "ejs");
+    app.set("views", path.join(__dirname, "views"));
+    app.use(express.urlencoded({ extended: true }));
+    app.engine("ejs",ejsMate);
+    app.use(express.static(path.join(__dirname,"public")));
+    const mongoUrl = "mongodb://127.0.0.1:27017/WanderLust";
+    app.listen(8080, (req, res) => {
+        console.log("Server is working on the port no 8080");
+    });
 
-async function main() {
-    mongoose.connect(mongoUrl);
-}
+    async function main() {
+        mongoose.connect(mongoUrl);
+    }
 
-main().then(() => {
-    console.log("Connected Successfully");
-}).catch((err) => {
-    console.log(err);
-});
+    main().then(() => {
+        console.log("Connected Successfully");
+    }).catch((err) => {
+        console.log(err);
+    });
 
-// app.get("/listing",async(req,res)=>
-// {
-//     let listing=new Listing({
-//         title:'Hello',
-//         description:'This is the villa',
-//         price:400,
-//         location:'Goa',
-//         country:'India'
-//     });
-//     await listing.save().then(()=>console.log("saved"));
-//     res.send("success");
-// })
+    // app.get("/listing",async(req,res)=>
+    // {
+    //     let listing=new Listing({
+    //         title:'Hello',
+    //         description:'This is the villa',
+    //         price:400,
+    //         location:'Goa',
+    //         country:'India'
+    //     });
+    //     await listing.save().then(()=>console.log("saved"));
+    //     res.send("success");
+    // })
 
-//Home Route
-app.get("/",(req,res)=>{
-    res.send("Hi i am root");
-});
+    //Home Route
+    app.get("/",(req,res)=>{
+        res.send("Hi i am root");
+    });
 
-//Index Route 
-app.get("/listing", async (req, res) => {
-    let alllisting = await Listing.find({});
-    res.render("listing/index", { alllisting });
-});
+    //Index Route 
+    app.get("/listing", async (req, res) => {
+        let alllisting = await Listing.find({});
+        res.render("listing/index", { alllisting });
+    });
 
-//New Route
-app.get("/listing/new",(req,res)=>
-{
-    res.render("listing/new");
-});
+    //New Route
+    app.get("/listing/new",(req,res)=>
+    {
+        res.render("listing/new");
+    });
 
-//Show Route
-app.get("/listing/:id", async (req, res) => {
- let{id}=req.params;
- let list=await Listing.findById(id);
- res.render("listing/show",{list});
-});
-
-//Create Route
-app.post("/listing",async(req,res)=>
-{
-    const newListing=new Listing(req.body.listing);
-    await newListing.save();
-    res.redirect("/listing");
-});
-
-//Edit Route
-app.get("/listing/:id/edit",async(req,res)=>
-{
+    //Show Route
+    app.get("/listing/:id", async (req, res) => {
     let{id}=req.params;
     let list=await Listing.findById(id);
-    res.render("listing/edit",{list});
-});
+    //  console.log(list);
+    res.render("listing/show",{list});
+    });
 
-//Update Route
-app.put("/listing/:id",async(req,res)=>{
-    let{id}=req.params;
-    await Listing.findByIdAndUpdate(id, {...req.body.listing});
-    res.redirect(`/listing/${id}`);
-});
+    //Create Route
+    app.post("/listing",async(req,res)=>
+    {
+        const newListing=new Listing(req.body.listing);
+        await newListing.save();
+        res.redirect("/listing");
+    });
 
-//Delete Route
-app.delete("/listing/:id",async(req,res)=>
-{
-    let{id}=req.params;
-    console.log(id);
-    let deletedItem=await Listing.findByIdAndDelete(id);
-    console.log(deletedItem);
-    res.redirect("/listing");
-});;
+    //Edit Route
+    app.get("/listing/:id/edit",async(req,res)=>
+    {
+        let{id}=req.params;
+        let list=await Listing.findById(id);
+        res.render("listing/edit",{list});
+    });
+
+    //Update Route
+    app.put("/listing/:id",async(req,res)=>{
+        let{id}=req.params;
+        await Listing.findByIdAndUpdate(id, {...req.body.listing});
+        res.redirect(`/listing/${id}`);
+    });
+
+    //Delete Route
+    app.delete("/listing/:id",async(req,res)=>
+    {
+        let{id}=req.params;
+        console.log(id);
+        let deletedItem=await Listing.findByIdAndDelete(id);
+        console.log(deletedItem);
+        res.redirect("/listing");
+    });;
 
--- models/listing.js
+++ models/listing.js
@@ -6,11 +6,18 @@
             required:true,
         },
         description:String,
-        image:{
-            type:Object,
-            default:"https://plus.unsplash.com/premium_photo-1687960116497-0dc41e1808a2?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            set:v=>(v===""?"default":v),
-        },
+        // image:{
+        //     type:Object,
+        //     hello:"https://images.unsplash.com/photo-1454388683759-ee76c15fee26?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        //     set:v=>(v===""?hello:v),
+        // },
+        image: {
+            type: Object,
+            default: {
+              url: "https://images.unsplash.com/photo-1454388683759-ee76c15fee26?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            },
+            set: (v) => (v === "" ? { url: "https://images.unsplash.com/photo-1454388683759-ee76c15fee26?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" } : v),
+          },
         price:Number,
         location:String,
         country:String
