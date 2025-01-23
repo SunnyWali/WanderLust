404bd224e699cf0c50908c1646d02f8e42a37b18
Sunny Wali <sunnywali777@gmail.com>
2025-01-23 21:14:58 +0545

    made some changes

--- models/listing.js
+++ models/listing.js
@@ -13,9 +13,9 @@
         // },
         image: {
             type: Object,
-            default: {
-              url: "https://images.unsplash.com/photo-1454388683759-ee76c15fee26?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            },
+            // default: {
+            //   url: "https://images.unsplash.com/photo-1454388683759-ee76c15fee26?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            // },
             set: (v) => (v === "" ? { url: "https://images.unsplash.com/photo-1454388683759-ee76c15fee26?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" } : v),
           },
         price:Number,
